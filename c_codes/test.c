5041136021577e277f542d093019b415348653cb
MarlonDdaSilva <marlonds1110@gmail.com>
2026-03-02 12:40:10 -0300

    Commit Inicial: Adicionados códigos teste em C e Java

--- c_codes/test.c
+++ c_codes/test.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h>
 int main() {
-  printf("Hello World!");
+  int numeros[5];
+  int i;
+  for (i = 0; i < 5; i++) {
+    printf("Escolha um núumero\n");
+    scanf("%d", &numeros[i]);
+  }
+  foreach()
   return 0;
 }
